57a993c6b77472fe2d90a328b60113358a253096
Muradawad20 <68800448+MuradAL-Tarifi@users.noreply.github.com>
2021-11-11 13:46:02 +0200

    Add Full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,50 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/Home/category/category.component';
+import { HomeComponent } from './components/Home/home/home.component';
+import { ProductComponent } from './components/Home/product/product.component';
+import { AboutComponent } from './components/Home/about/about.component';
+import { ContactComponent } from './components/Home/contact/contact.component';
+import { FooterComponent } from './components/Home/footer/footer.component';
+import { AddressComponent } from './components/Account/address/address.component';
+import { AccountComponent } from './components/Account/account/account.component';
+import { ChangePasswordComponent } from './components/Account/change-password/change-password.component';
+import { DrugsComponent } from './components/Account/drugs/drugs.component';
+import { MyDrugsComponent } from './components/Account/my-drugs/my-drugs.component';
+import { OrdersComponent } from './components/Account/orders/orders.component';
+import { WaterComponent } from './components/Account/water/water.component';
+import { WishlistComponent } from './components/Account/wishlist/wishlist.component';
+import { OrderDetailsComponent } from './components/Account/order-details/order-details.component';
+import { IndexComponent } from './components/Index/index/index.component';
+import { CartComponent } from './components/Shop/cart/cart.component';
+import { CheckOutComponent } from './components/Shop/check-out/check-out.component';
+import { ProductSingleComponent } from './components/Shop/product-single/product-single.component';
+import { ThankYouComponent } from './components/Shop/thank-you/thank-you.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    HomeComponent,
+    ProductComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    AddressComponent,
+    AccountComponent,
+    ChangePasswordComponent,
+    DrugsComponent,
+    MyDrugsComponent,
+    OrdersComponent,
+    WaterComponent,
+    WishlistComponent,
+    OrderDetailsComponent,
+    IndexComponent,
+    CartComponent,
+    CheckOutComponent,
+    ProductSingleComponent,
+    ThankYouComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/Account/my-drugs/my-drugs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-drugs',
+  templateUrl: './my-drugs.component.html',
+  styleUrls: ['./my-drugs.component.css']
+})
+export class MyDrugsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
