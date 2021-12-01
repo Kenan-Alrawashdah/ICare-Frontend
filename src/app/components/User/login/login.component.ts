51908d46a63be503d198116d80faa8d420b30192
Muradawad20 <68800448+MuradAL-Tarifi@users.noreply.github.com>
2021-12-01 20:51:45 +0200

    ..

--- src/app/components/User/login/login.component.ts
+++ src/app/components/User/login/login.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Constants } from 'src/app/Helper/constants';
 import { UserService } from 'src/app/services/user.service';
-
+declare var FB: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -19,8 +19,28 @@ export class LoginComponent implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
     });
-  }
+    (window as any).fbAsyncInit = function () {
+      FB.init({
+        appId: '1291083414713212',
+        cookie: true,
+        xfbml: true,
+        version: 'v12.0',
+      });
+      FB.AppEvents.logPageView();
+    };
 
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  }
   onSubmit() {
     let email = this.loginForm.controls['email'].value;
     let password = this.loginForm.controls['password'].value;
@@ -52,5 +72,12 @@ export class LoginComponent implements OnInit {
         console.log('error', error);
       }
     );
+  }
+  submitLogin() {
+    console.log('submit login to facebook');
+    // FB.login();
+    FB.login((response) => {
+      console.log('submitLogin', response);
+    });
   }
 }
