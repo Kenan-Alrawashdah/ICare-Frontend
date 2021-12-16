328148655dc608cc3e584e0c251e8d9c6160bd95
Muradawad20 <68800448+MuradAL-Tarifi@users.noreply.github.com>
2021-12-16 12:20:06 +0200

    ..

--- src/app/components/delivery-dashboard/avilable-orders/avilable-orders.component.ts
+++ src/app/components/delivery-dashboard/avilable-orders/avilable-orders.component.ts
@@ -6,34 +6,41 @@ import { DeliveryService } from '../service/delivery.service';
 @Component({
   selector: 'app-avilable-orders',
   templateUrl: './avilable-orders.component.html',
-  styleUrls: ['./avilable-orders.component.css']
+  styleUrls: ['./avilable-orders.component.css'],
 })
 export class AvilableOrdersComponent implements OnInit {
-
-  ordersList:PlacedLocationModel[]
+  ordersList: PlacedLocationModel[];
   closeResult: string = '';
   lat = '';
   lng = '';
   Locatins;
+  ds: google.maps.DirectionsService;
+  dr: google.maps.DirectionsRenderer;
+  source: google.maps.LatLngLiteral;
+  destination: google.maps.LatLngLiteral;
 
   constructor(
-    private deliveryService:DeliveryService,
+    private deliveryService: DeliveryService,
     private modalService: NgbModal
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.getLocation()
+    this.getLocation();
+    this.ds = new google.maps.DirectionsService();
+    this.dr = new google.maps.DirectionsRenderer({
+      map: null,
+      suppressMarkers: true,
+    });
   }
 
-  async getLocation()
-  {
-    await this.deliveryService.GetPlacedOrders().toPromise()
-    .then(
-      (response)=>{
+  async getLocation() {
+    await this.deliveryService
+      .GetPlacedOrders()
+      .toPromise()
+      .then((response) => {
         console.log(response);
         this.ordersList = response.data;
-      }
-    )
+      });
   }
 
   ChangeMapLocation(lat, lng) {
@@ -44,7 +51,6 @@ export class AvilableOrdersComponent implements OnInit {
       {
         zoom: 15,
         center: { lat: lat, lng: lng },
-       
       }
     );
     const geocoder = new google.maps.Geocoder();
@@ -57,34 +63,77 @@ export class AvilableOrdersComponent implements OnInit {
     map: google.maps.Map,
     infowindow: google.maps.InfoWindow
   ) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.source = {
+        lat: parseFloat('40.714224'),
+        lng: parseFloat('-73.991452'),
+      };
+    });
     const latlng = {
       lat: parseFloat(this.lat),
       lng: parseFloat(this.lng),
     };
+    this.destination = latlng;
     geocoder
       .geocode({ location: latlng })
       .then((response) => {
         if (response.results[0]) {
           map.setZoom(11);
 
-          const marker = new google.maps.Marker({
-            position: latlng,
+          var markerStart = new google.maps.Marker({
+            position: this.source,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            icon: {
+              url: './assets/map/truck_pin.svg',
+              anchor: new google.maps.Point(35, 10),
+              scaledSize: new google.maps.Size(100, 100),
+            },
             map: map,
           });
+          const destinationMarker = new google.maps.Marker({
+            position: this.destination,
+            map: map,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            icon: {
+              url: './assets/map/destination_custom_pin.svg',
+              anchor: new google.maps.Point(35, 10),
+              scaledSize: new google.maps.Size(100, 100),
+            },
+          });
 
+          map.panTo(this.destination);
+          this.setRoutePolyline(map);
           infowindow.setContent(response.results[0].formatted_address);
-          infowindow.open(map, marker);
+          infowindow.open(map, destinationMarker);
         } else {
           window.alert('No results found');
         }
       })
       .catch((e) => window.alert('Geocoder failed due to: ' + e));
   }
+  setRoutePolyline(map: google.maps.Map) {
+    let request = {
+      origin: this.source,
+      destination: this.destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+    };
+
+    this.ds.route(request, (response, status) => {
+      this.dr.setOptions({
+        suppressPolylines: false,
+        map: map,
+      });
+
+      if (status == google.maps.DirectionsStatus.OK) {
+        this.dr.setDirections(response);
+      }
+    });
+  }
 
   async open(content: any, id: number) {
-  
-    
-    this.Locatins = this.ordersList.find(o=>o.orderId == id)
+    this.Locatins = this.ordersList.find((o) => o.orderId == id);
     this.modalService
       .open(content, { ariaLabelledBy: 'modal-basic-title' })
       .result.then((result) => {
@@ -92,14 +141,9 @@ export class AvilableOrdersComponent implements OnInit {
       });
   }
 
-  takeOrder(id:number)
-  {
-    this.deliveryService.Takeorder(id).subscribe(
-      (response)=>
-      {
-        this.ngOnInit();
-      }
-    )
+  takeOrder(id: number) {
+    this.deliveryService.Takeorder(id).subscribe((response) => {
+      this.ngOnInit();
+    });
   }
-
 }
