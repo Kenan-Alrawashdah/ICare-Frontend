68864ae6583d9db96bbe45492dcabb0e5d62f070
Muradawad20 <68800448+MuradAL-Tarifi@users.noreply.github.com>
2021-12-21 20:29:08 +0200

    ..

--- src/app/components/delivery-dashboard/delivery-orders/delivery-orders.component.ts
+++ src/app/components/delivery-dashboard/delivery-orders/delivery-orders.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AllOrdersForDelivery } from 'src/app/shared/Delivery/all-orders-for-delivery.model';
 import { DashboardService } from 'src/app/shared/Delivery/dashboard.service';
@@ -23,6 +23,11 @@ export class DeliveryOrdersComponent implements OnInit {
   lng = '';
   Locatins;
   NumberOfOrders: number;
+  map: google.maps.Map;
+  ds: google.maps.DirectionsService;
+  dr: google.maps.DirectionsRenderer;
+  source: google.maps.LatLngLiteral;
+  destination: google.maps.LatLngLiteral;
 
   constructor(
     private deliveryService: DeliveryService,
@@ -31,6 +36,19 @@ export class DeliveryOrdersComponent implements OnInit {
 
   ngOnInit(): void {
     this.getMyOrders();
+    console.log('sad');
+    this.map = new google.maps.Map(
+      document.getElementById('map') as HTMLElement,
+      {
+        center: { lat: 31.963158, lng: 35.930359 },
+      }
+    );
+
+    this.ds = new google.maps.DirectionsService();
+    this.dr = new google.maps.DirectionsRenderer({
+      map: this.map,
+      suppressMarkers: true,
+    });
   }
 
   async getMyOrders() {
@@ -42,26 +60,33 @@ export class DeliveryOrdersComponent implements OnInit {
         this.NumberOfOrders = this.myOrders.length;
       });
   }
-
   ChangeMapLocation(lat, lng) {
     this.lat = lat;
     this.lng = lng;
-    const map = new google.maps.Map(
+    this.map = new google.maps.Map(
       document.getElementById('map') as HTMLElement,
       {
         zoom: 15,
         center: { lat: lat, lng: lng },
       }
     );
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.source = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
     const geocoder = new google.maps.Geocoder();
     const infowindow = new google.maps.InfoWindow();
-    this.geocodeLatLng(geocoder, map, infowindow);
+    this.geocodeLatLng(geocoder, this.map, infowindow, lat, lng);
   }
 
   geocodeLatLng(
     geocoder: google.maps.Geocoder,
     map: google.maps.Map,
-    infowindow: google.maps.InfoWindow
+    infowindow: google.maps.InfoWindow,
+    lat,
+    lng
   ) {
     const latlng = {
       lat: parseFloat(this.lat),
@@ -71,20 +96,60 @@ export class DeliveryOrdersComponent implements OnInit {
       .geocode({ location: latlng })
       .then((response) => {
         if (response.results[0]) {
-          map.setZoom(11);
-
-          const marker = new google.maps.Marker({
-            position: latlng,
+          new google.maps.Marker({
+            position: this.source,
+            animation: google.maps.Animation.DROP,
+            icon: {
+              url: './assets/map/truck_pin.svg',
+              anchor: new google.maps.Point(35, 10),
+              scaledSize: new google.maps.Size(100, 100),
+            },
             map: map,
           });
+          const latlng = {
+            lat: parseFloat(this.lat),
+            lng: parseFloat(this.lng),
+          };
+          this.destination = latlng;
 
-          infowindow.setContent(response.results[0].formatted_address);
-          infowindow.open(map, marker);
+          new google.maps.Marker({
+            position: this.destination,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: {
+              url: './assets/map/destination_custom_pin.svg',
+              anchor: new google.maps.Point(35, 10),
+              scaledSize: new google.maps.Size(100, 100),
+            },
+          });
+          map.panTo(this.destination);
+          this.setRoutePolyline(map);
+          console.log(this.destination);
+          console.log(this.source);
         } else {
           window.alert('No results found');
         }
       })
       .catch((e) => window.alert('Geocoder failed due to: ' + e));
+  }
+
+  setRoutePolyline(map: google.maps.Map) {
+    let request = {
+      origin: this.source,
+      destination: this.destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+    };
+
+    this.ds.route(request, (response, status) => {
+      this.dr.setOptions({
+        suppressPolylines: false,
+        map: map,
+      });
+
+      if (status == google.maps.DirectionsStatus.OK) {
+        this.dr.setDirections(response);
+      }
+    });
   }
 
   async open(content: any, id: number) {
